3ac3b7ab954056050e36e94ab58e8679cac5272c
Chandu <chandra.vedantham@gmail.com>
2014-05-25 23:15:22 -0400

    Basic implementation of concat plugin for pvc

---
+++ src/PvcConcat.Specs/PvcConcat.Specs/PvcConcatSpecs.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Machine.Specifications;
+using PvcCore;
+
+namespace PvcConcat.Specs
+{
+	[Subject(typeof (PvcConcat), ".Execute")]
+	internal class PvcConcatSpecs
+	{
+		public class When_the_input_streams_have_content
+		{
+			private static PvcConcat _pvcConcat;
+			private static IEnumerable<PvcStream> _inputStreams;
+			private static IEnumerable<PvcStream> _outputStreams;
+
+			private Establish context = () =>
+			{
+				_pvcConcat = new PvcConcat("somefile.txt");
+				_inputStreams = new[]
+				{
+					PvcUtil.StringToStream("File1Content", "File1", "C:\\File1.txt"),
+					PvcUtil.StringToStream("File2Content", "File2", "D:\\File2.txt"),
+				};
+			};
+
+			private Because of = () =>
+				_outputStreams = _pvcConcat.Execute(_inputStreams);
+
+			private It should_return_streams_with_one_entry = () =>
+				_outputStreams.Count().ShouldEqual(1);
+
+			private It should_merge_input_streams_to_one_stream = () =>
+			{
+				var output = _outputStreams.First();
+				new StreamReader(output).ReadToEnd().ShouldEqual("File1ContentFile2Content");
+			};
+				
+		}
+	}
+}
---
+++ src/PvcConcat/PvcConcat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using PvcCore;
+using PvcPlugins;
+
+namespace PvcConcat
+{
+	public class PvcConcat : PvcPlugin
+	{
+		private readonly string _resultName;
+
+		public PvcConcat(string resultName)
+		{
+			_resultName = resultName;
+		}
+
+		public override IEnumerable<PvcStream> Execute(IEnumerable<PvcStream> inputStreams)
+		{
+			var streamContent = new StringBuilder();
+			foreach (var inputStream in inputStreams)
+			{
+				streamContent.Append(inputStream.ToString());
+			}
+			var finalStream = PvcUtil.StringToStream(streamContent.ToString(), _resultName);
+			finalStream.Position = 0;
+			return new[]
+			{
+				finalStream
+			};
+
+		}
+	}
+}
